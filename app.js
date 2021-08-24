1df21ec40c8f2f924b5e8226f734428a852a068c
chinmay <chinmaymhatre12@gmail.com>
2021-08-25 00:32:37 +0530

    create db connection

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const dotenv = require("dotenv")
-
+const connect = require('./config/db')
 dotenv.config({path:"./config/config.env"})
+
+const app = express()
+connect()
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("listening at port 5000")
+})
