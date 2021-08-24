29284b61100b3bc0ae4b80dcda835a0a11a6a23b
chinmay <chinmaymhatre12@gmail.com>
2021-08-24 23:56:22 +0530

    finished all schemas

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const mongoose  = require('mongoose')
+const Schema    = mongoose.Schema
+
+const productSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true,
+        maxLength:32
+    },
+    description:{
+        type:String,
+        trim:true,
+        required:true,
+        maxLength:2000
+    },
+    price:{
+        type:Number,
+        required:true,
+        maxLength:32,
+        trim:true
+    },
+    category:{
+        type:Schema.Types.ObjectId,
+        ref:'Category',
+        require:true
+    },
+    stock:{
+        type:Number,
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+    photo:{
+        data:buffer,
+        contentType:String
+    }
+},{timestamps:true})
+
+mondule.exports = mongoose.model('Product',productSchema)
