e0011891b6f44299574273361d36bfc075e77bb3
chinmay <chinmaymhatre12@gmail.com>
2021-08-25 10:41:30 +0530

    add basic middlewares

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const dotenv = require("dotenv")
-const connect = require('./config/db')
+const express       = require('express')
+const dotenv        = require("dotenv")
+const connect       = require('./config/db')
+const cookieParser  = require('cookie-parser')
+const cors           = require('cors')
+const app           = express()
 dotenv.config({path:"./config/config.env"})
 
-const app = express()
+//* DB Connect 
 connect()
 
+//* Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+//* Routes 
 app.get('/',(req,res)=>{
     res.send("hello")
 })
