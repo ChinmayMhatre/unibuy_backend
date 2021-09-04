cee18d046ecb747c897aad893c8f652852ed2f89
chinmay <chinmaymhatre12@gmail.com>
2021-09-04 16:40:15 +0530

    update route in progress

--- controllers/user.js
+++ controllers/user.js
@@ -26,8 +26,43 @@ exports.getUser = async (req,res)=>{
     req.profile.createdAt = undefined;
     req.profile.updatedAt = undefined;
 
-    return res.json({
+    return res.status(200).json({
         success:true,
         data : req.profile
     })
+}
+
+exports.updateUser = async (req,res)=>{
+    if(req.body.hasOwnProperty("salt")){
+        return res.status(403).json({
+            success:false,
+            error:"you can't do that"
+        })
+    }
+    if(req.body.hasOwnProperty("encrypt_password")){
+        return res.status(403).json({
+            success:false,
+            error:"you can't do that"
+        })
+    }
+    try {
+        let user = await User.findByIdAndUpdate({_id:req.profile._id},
+            req.body,
+            {new:true,useFindAndModify:false}
+            )
+            console.log("HEllo")
+            user.salt = undefined;
+            user.encrypt_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+        return res.status(200).json({
+            success:true,
+            data : user
+        })    
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            error:error
+        })
+    }
 }
